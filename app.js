321f1a422f6f67ad59c857c1eb908183e39fa584
clementrozario <clementcla@outlook.com>
2023-10-16 10:05:41 +0530

    payment integration in expense app

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var cors = require('cors');
+const sequelize = require('./util/database');
+const User = require('./models/user');
+const Expense = require('./models/expenses');
+const Order = require('./models/orders');
+
+
+const userRoutes = require('./routes/user');
+const expenseRoutes = require('./routes/expense');
+const purchaseRoutes = require('./routes/purchase')
+
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', userRoutes);
+app.use('/expense', expenseRoutes);
+app.use('/purchase', purchaseRoutes);
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
+
+sequelize.sync({force:true}).then(() => {
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+}).catch(err => {
+    console.log(err);
+});
